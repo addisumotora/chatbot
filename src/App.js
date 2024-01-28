274be364d3ae30135b52193e4bb6a9ff6a2b7c79
addisumotora <addisumotora3@gmail.com>
2024-01-29 01:40:59 +0300

    registeration and reset password page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import Login from "./components/Login";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Register from "./components/Register";
+import ResetPassword from "./components/ResetPassword";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" exact element={<Login />} />
-        <Route path="chat" element={<Login />} />
+        <Route path="/chat" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </Router>
   );
